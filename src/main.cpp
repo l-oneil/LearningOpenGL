e03ae2b96940baf9e31b858b0a1024e855974fad
l-oneil <60259140+l-oneil@users.noreply.github.com>
2021-09-02 22:55:15 +0100

    tidy up;

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
-// unsigned int loadTexture(const char *path);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
